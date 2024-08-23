92c5a64b1a8665ae902ed7565acc659e51529459
tcly <yiji.wang@ly.com>
2024-08-23 13:32:11 +0800

    add commit

--- Learngit/Learngit/ViewController.swift
+++ Learngit/Learngit/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class ViewController: UIViewController {
 	//d
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad()//
         // Do any additional setup after loading the view.
     }
 
